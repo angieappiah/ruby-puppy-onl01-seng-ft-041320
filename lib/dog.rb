db3764aba2b1bb899cd3ed59a87b0845f16ae544
Angela Appiah <alegnalawn2@yahoo.com>
2020-04-23 20:47:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,21 +7,27 @@ class Dog
     @name = name
   end
 
-  def self.all
-     @@all
-   end
+  
+    def self.all
+      @@all
+    end
 
-   def save
-    @@all << self
+    def self.clear_all
+      @@all = [] 
+    end
+
+    def self.print_all
+      @@all.each do |dog|
+        puts dog.name
+      end
   end
 
-   def self.clear_all
-       @@all = []
-   end
+    def save
+      @@all << self
+    end
 
-   def self.print_all
-   @@all.each do |dog|
-     puts dog.name
-   end
-   
+    def name
+      @name
+    end
+
 end
