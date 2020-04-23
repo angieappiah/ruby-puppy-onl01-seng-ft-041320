8aa698e0ccff1fcfb1e92edae31837feddfce1e1
Angela Appiah <alegnalawn2@yahoo.com>
2020-04-23 20:42:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,15 @@
 # Add your code here
 class Dog
   attr_accessor :name
-  @@name = []
+  @@all = []
 
   def initialize (name)
     @name = name
-    @@name << name
+    self.save
   end
 
 def self.name
-@@name
+  @@name
 end
 
 
